033e2544c76dfb52f92a6e0429aea0fe6e4ce9a8
Bhavesh <bhaveshnankani55@gmail.com>
2025-06-19 18:46:31 +0100

    Project ready for deployment

--- testflask.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "http://localhost:5000/chat"
-headers = {"Content-Type": "application/json"}
-data = {"message": "Hello"}
-
-response = requests.post(url, json=data, headers=headers)
-print(response.status_code, response.json())  # Debug Output
