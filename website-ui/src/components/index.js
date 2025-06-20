15a57a6dfacd3b5482cf1af79c5943043ff251c4
Bhavesh <bhaveshnankani55@gmail.com>
2025-06-20 18:57:48 +0100

    Fixing erros to deployment

---
+++ website-ui/src/components/index.js
@@ -0,0 +1,8 @@
+// Export all components from this file
+export { default as Categories } from './Categories';
+export { default as ChatInterface } from './ChatInterface';
+export { default as ProductCard } from './Products/ProductCard';
+export { default as ProductList } from './Products/ProductList';
+export { default as Navbar } from './Navigation/Navbar';
+export { default as SearchBar } from './Navigation/SearchBar';
+export { default as Footer } from './UI/Footer';
